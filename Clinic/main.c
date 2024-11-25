44d018d601e08642fa628a0db87649708e56c555
Mostafa Fathy Ali Amer <96601097+tofyfathy12@users.noreply.github.com>
2024-11-25 16:25:52 +0200

    .

--- Clinic/main.c
+++ Clinic/main.c
@@ -5,5 +5,9 @@ int main()
 {
     printf("this message is from the main file\n");
     printf("Mostafa\n");
+    for (int i = 0; i < 1000; i++)
+    {
+        printf("%d\n", i+1);
+    }
     return 0;
 }
